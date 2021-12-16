d4a9f7bc2f53e36792a4582aec24605484730d30
Darius Kharazi <dkharazif@gmail.com>
2021-12-16 14:10:07 -0500

    Update purge-logs.sh

--- purge-logs.sh
+++ purge-logs.sh
@@ -1,10 +1,8 @@
 # Copy log contents to temp file
-cp /home/dkharazif/typesense-server-data/typesense-server-data.log /home/dkharazif/typesense-server-data/typesense-
-server-data-temp.log
+cp /home/dkharazif/typesense-server-data/typesense-server-data.log /home/dkharazif/typesense-server-data/typesense-server-data-temp.log
 
 # Print last 100 lines to log file
-tail -100 /home/dkharazif/typesense-server-data/typesense-server-data-temp.log > /home/dkharazif/typesense-server-d
-ata/typesense-server-data.log
+tail -100 /home/dkharazif/typesense-server-data/typesense-server-data-temp.log > /home/dkharazif/typesense-server-data/typesense-server-data.log
 
 # Delete temp file
 rm /home/dkharazif/typesense-server-data/typesense-server-data-temp.log
